41d249a4651e64662ef8c62a3f54f40d87776db5
Alex Beyer <alex.beyer.villarreal@gmail.com>
2026-02-18 12:03:59 -0600

    Updated it again. Still not functional

--- Python Learning/Protean_cypher.py
+++ Python Learning/Protean_cypher.py
@@ -6,9 +6,10 @@ double_team = random.choice(chaos_characters)
 
 def Protean_encryption(message):
     print("Welcome to Protean Cipher! Let's encrypt your message with a unique twist!")
-    input("Enter whatever you wish to encrypt: ")
     message = ""
+    double_team = message + random.choice(chaos_characters)
     print(f"Your message is: {message}")
     print(f"Your encryption key is: {double_team}")
-    
 
+
+Protean_encryption(message = input("Enter your message: "))
