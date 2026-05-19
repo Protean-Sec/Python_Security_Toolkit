96cf631eb289aecc9b938f2bbb27f856161971fb
Alex Beyer <alex.beyer.villarreal@gmail.com>
2026-05-19 16:28:42 -0600

    Add file sorter script

---
+++ Python Learning/cyphers/file_sorter.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+from pathlib import Path
+# Path.home() finds your user folder (e.g., C:/Users/Name), 
+# and / "Downloads" adds the specific folder we want to organize.
+target_dir = Path.home() / "Downloads"
+#Dictionary for where we want to sort each file type and in what folder
+file_map = {
+    ".jpeg" : "Pictures",
+    ".jpg"  : "Pictures",
+    ".pdf"  : "Documents",
+    ".mp4"  : "Videos",
+    ".png"  : "Pictures",
+    ".mov"  : "Videos",
+    ".heic" : "Pictures",
+    ".docx" : "Documents",
+    ".mp3"  : "Audio",  
+    ".m4a"  : "Audio",
+    ".wav"  : "Audio",
+    ".zip"  : "Archives",
+    ".exe"  : "Archives",
+    ".msi"  : "Archives",
+    ".rar"  : "Archives.",
+    ".pptx" : "Documents",
+    ".xlsx" : "Documents",
+
+}
+def organize_folder ():
+    
+    for file in target_dir.iterdir(): 
+        # .iterdir() allows us to loop through every item inside the target directory.
+        if file.is_file():
+        # Move only files
+
+           extension = file.suffix.lower()
+           # Get the extension (ending) of each file: .pdf, .jpg, etc.
+
+           if extension in file_map:
+             # If the extension is in our dictionary, start the move process.
+             folder_name = file_map[extension]
+
+             # Construct the full path for the destination (Downloads/FolderName).
+             dest_path = target_dir / folder_name
+
+             dest_path.mkdir(exist_ok=True)
+             # Check if that folder exists, and if not, create it.
+             # exist_ok = True  prevents an error if the folder is already there
+
+             shutil.move(str(file), str(dest_path / file.name))
+             # first argument is the source, the second is the destination folder
+             print(f"Moved {file.name} to {folder_name}")
+
+
+if __name__ == "__main__":
+    organize_folder()
+
+
+            
+
+
+        
+
